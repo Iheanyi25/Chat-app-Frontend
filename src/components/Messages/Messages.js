cde8a13640c38a86423b3116992b28debe7290ce
Iheanyi25 <ihekweabai@gmail.com>
2020-08-04 14:26:45 +0100

    Changed the folder structure of the Message component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -3,7 +3,7 @@ import React from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 
 import "./Messages.css";
-import Message from "../Message/Message";
+import Message from "./Message/Message";
 
 const Messages = ({ messages, name }) => {
   return (
